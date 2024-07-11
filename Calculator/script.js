188bab323e61b397fd03bbe78a3df0070c7878d7
Nikita Saini <nikitasaini0106@gmail.com>
2024-07-11 19:35:03 +0530

    Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,30 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == '←') {
+            string = string.slice(0, -1);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == '+/-') {
+            if (string.startsWith("-")) {
+                string = string.substring(1);
+            } else {
+                string = "-" + string;
+            }
+            document.querySelector('input').value = string;
+        }
+        else {
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    });
+});
